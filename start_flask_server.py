8f9b68a729cf94dba28bc86c11745577c5c45757
matt <biondina.matthijs@gmail.com>
2023-11-07 18:13:03 +0100

    sockets working on one PC

--- start_flask_server.py
+++ start_flask_server.py
@@ -1,12 +1,9 @@
 import socket
 
-from flask_model_cache.flask_server import flask_server_startup
 from flask_model_cache.pretrained_model_server import ModelServer
 
 MODEL_PATH = "/home/matt/Models/epoch=14-step=5610.ckpt"
 
-if __name__ == '__main__':
-    flask_server_startup(model_path=MODEL_PATH)
 if __name__ == '__main__':
     if socket.gethostname() == 'kat':
         ms = ModelServer("172.18.20.240", 5001)
